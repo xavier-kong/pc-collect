56dba9648391773d415fc97f33b4313f99373f11
xavier-kong <kongxianchengxavier@gmail.com>
2023-08-25 21:58:11 +0800

    ok now it should :

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -3,13 +3,10 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+  .input(z.object({ text: z.string() }))
+  .query(({ input }) => {
+    return {
+      greeting: `Hello ${input.text}`,
+    };
   }),
-});
+ });
